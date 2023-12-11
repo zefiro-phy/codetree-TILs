c00febe89dd95d5a3dde0c41b3358d3603ecac92
zefiro-phy <153213510+zefiro-phy@users.noreply.github.com>
2023-12-11 17:21:33 +0900

    Title: 두 사람, Time: 110ms, Memory: 24MB, Status: Passed - CodeTree

--- 231211/두 사람/two-person.py
+++ 231211/두 사람/two-person.py
@@ -2,7 +2,7 @@ a = input().split()
 b = input().split()
 a_a = int(a[0])
 b_a = int(b[0])
-if (a_a>=19 and a[1]="M")or(b_a>= 19 and b[1]="M"):
+if (a_a>=19 and a[1]=="M")or(b_a>= 19 and b[1]=="M"):
     print(1)
 else:
     print(0)
