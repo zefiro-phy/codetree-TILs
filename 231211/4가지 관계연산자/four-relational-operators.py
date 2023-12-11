a68919bd58b252f963506ca27bff4da41c53cea0
zefiro-phy <153213510+zefiro-phy@users.noreply.github.com>
2023-12-11 14:28:05 +0900

    Title: 4가지 관계연산자, Time: 162ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 231211/4가지 관계연산자/four-relational-operators.py
@@ -0,0 +1,22 @@
+ar = input().split()
+a = int(ar[0])
+b = int(ar[1])
+if a>= b:
+    print(1)
+else:
+    print(0)
+
+if a>b:
+    print(1)
+else:
+    print(0)
+
+if b>= a:
+    print(1)
+else:
+    print(0)
+
+if b>a:
+    print(1)
+else:
+    print(0)
