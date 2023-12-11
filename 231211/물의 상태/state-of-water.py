adc69edb0a57d6897a140d00931e21aea9644aca
zefiro-phy <153213510+zefiro-phy@users.noreply.github.com>
2023-12-11 14:18:56 +0900

    Title: 물의 상태, Time: 105ms, Memory: 24MB, Status: Passed - CodeTree

--- 231211/물의 상태/state-of-water.py
+++ 231211/물의 상태/state-of-water.py
@@ -1,7 +1,7 @@
 temp= int(input())
 if temp<0:
     print("ice")
-elif temp > 100:
+elif temp >= 100:
     print("vapor")
-elif temp >0 and temp <100:
+elif temp >=0 and temp <100:
     print("water")
