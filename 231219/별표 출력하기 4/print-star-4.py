3d81abe9d10be86bddaffdae9571de14f7bb2aef
zefiro-phy <153213510+zefiro-phy@users.noreply.github.com>
2023-12-19 01:04:55 +0900

    Title: 별표 출력하기 4, Time: 114ms, Memory: 24MB, Status: Passed - CodeTree

--- 231219/별표 출력하기 4/print-star-4.py
+++ 231219/별표 출력하기 4/print-star-4.py
@@ -3,7 +3,7 @@ for i in range(n):
     for j in range (n-i):
         print("*", end = " ")
     print()
-for i in range(n-1,-1,-1):
+for i in range(n-2,-1,-1):
     for j in range (n-i):
         print("*", end = " ")
     print()
