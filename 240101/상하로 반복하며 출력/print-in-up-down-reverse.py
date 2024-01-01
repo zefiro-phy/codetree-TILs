14973521ab8cea5ffaf4e7d6029597b72c36b598
zefiro-phy <153213510+zefiro-phy@users.noreply.github.com>
2024-01-01 23:01:50 +0900

    Title: 상하로 반복하며 출력, Time: 123ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240101/상하로 반복하며 출력/print-in-up-down-reverse.py
@@ -0,0 +1,9 @@
+n = int(input())
+
+for i in range(1,n+1):
+    for j in range(1,n+1):
+        if j %2 == 1:
+            print(i, end = "")
+        else:
+            print(n-i+1, end = "")
+    print()
